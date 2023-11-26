3f1ed2ac87fcc741223868f1a137ad88fed6f847
KrishnaVaishnav98 <krishnavaishnav125@gmail.com>
2023-11-26 15:23:26 +0530

    filters complete

--- src/Components/Filters.jsx
+++ src/Components/Filters.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect } from 'react';
 import { GoSortAsc, GoSortDesc } from "react-icons/go";
 import { IoCloseCircleSharp } from "react-icons/io5";
-import { FaBurger, FaPizzaSlice, FaBowlFood } from "react-icons/fa6";
+import { FaBowlFood, FaPizzaSlice } from "react-icons/fa6";
 import { LuSalad } from "react-icons/lu";
+import { GiBowlOfRice } from "react-icons/gi";
 
 export const Filters = ({ query, setQuery, sort, handleSortAsc, handleSortDesc, removeSort }) => {
 
@@ -12,17 +13,17 @@ export const Filters = ({ query, setQuery, sort, handleSortAsc, handleSortDesc, 
         <>
             <div className='bg-white lg:flex justify-between h-[100px] lg:h-[50px] lg:sticky top=[100px] lg:top-[88px] z-50 bg-opacity-70 items-center' >
                 <div className='w-4/5 flex gap-4 justify-betweet items-center px-8 h-[50px] lg:h-[100px]'>
-                    <div className='flex gap-2 items-center cursor-pointer' onClick={() => setQuery('burger')}>
-                        <FaBurger color={'#8D3838'} />
-                        <p>Burgers</p>
-                    </div>
                     <div className='flex gap-2 items-center cursor-pointer' onClick={() => setQuery('salad')}>
                         <LuSalad color={'#8D3838'} />
                         <p>Salad</p>
                     </div>
-                    <div className='flex gap-2 items-center cursor-pointer' onClick={() => setQuery('pizza')}>
-                        <FaPizzaSlice color={'#8D3838'} />
-                        <p>Pizza</p>
+                    <div className='flex gap-2 items-center cursor-pointer' onClick={() => setQuery('bowl')}>
+                        <FaBowlFood color={'#8D3838'} />
+                        <p>Veg Bowl</p>
+                    </div>
+                    <div className='flex gap-2 items-center cursor-pointer' onClick={() => setQuery('pasta')}>
+                        <GiBowlOfRice color={'#8D3838'} />
+                        <p>Pasta</p>
                     </div>
                     <div className='flex gap-2 items-center cursor-pointer' onClick={() => setQuery('')}>
                         <FaBowlFood color={'#8D3838'} />
